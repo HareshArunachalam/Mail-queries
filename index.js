74abe6ff0f9d081195fd301c3996b1f4c85bd70d
HareshArunachalam <31510135+HareshArunachalam@users.noreply.github.com>
2017-12-14 10:46:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+//add these lines in your index.js
+var myModule = require('./module'); //mention the location correctly here, I am having the index.js and the module.js in the same folder and this is how it will look.
+myModule.function1() //to do your MQTT functions and make sure these functions are called inside the intent you want.
---
+++ module.js
@@ -0,0 +1,8 @@
+//Require all modules
+var module = require('Your Module')//in your case the MQTT module, also make sure you add it is added along with the zip file that we will attach and also dont forget to access it from the right location.  
+
+function 1{ 
+	//all the code that handles your MQTT functionalities
+}
+
+module.exports.function1 = function1; //export your function so that it can be accessed in your index.js
